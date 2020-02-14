62fc36cf1995905224e6bf4e68bd5c5e9130f232
Pragadeesh Sekar <praga63@gmail.com>
2020-02-14 16:16:55 +0530

    promise-for-loop in typescript for protractor

---
+++ promise-for-loop.ts
@@ -0,0 +1,27 @@
+/* Delete first element on page and confirm
+   Normal for loop runs faster & it does not wait for promise result 
+   Hence i used async and await , which worked exactly what i was looking for */ 
+const delCheck = async (check : any) {
+  for (var obj in check) {
+    await element.all(by.tagName('basic').get(1).click().then(() => {
+      element(by.buttonText('Delete');
+      element(by.buttonText('Confirm');
+    });
+  }
+}
+
+// Range function in typescript
+// eg : range(1, 3) ==> [1,2]
+function range(start, end) {
+  return Array(end - start).fill(0).map((_, idx) => start + idx)
+}
+
+// get count of basic tags present & delete them sequentially 
+element.all(by.tagName('basic')).count().then((cnt) => {
+  var check = range(1, cnt)
+  delCheck(check);
+});
+
+
+
+
